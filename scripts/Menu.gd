bc1e26ff7cb2eaf1ae0304bd23a4d7cedbe11a31
ANNA_ARAGAO <anna.aragao@sou.inteli.edu.br>
2024-02-08 10:32:08 -0300

    feat: added menu screen
    Added code and assets for background, text and buttons

---
+++ scripts/Menu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+#When button is pressed, it changes to Main Scene
+func _on_Button_pressed():
+	pass #get_tree().change_scene("")
+	
+	
+#When button is pressed, it ends the application 
+func _on_Button2_pressed():
+	get_tree().quit()
+	
+
+func _on_AudioStreamPlayer_ready():
+	$AudioStreamPlayer.play()
